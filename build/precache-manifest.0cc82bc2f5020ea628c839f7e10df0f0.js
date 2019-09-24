718a8a0089fd85cf2d8ac5995fc22ab2365eb2d4
yang <52070001@qq.com>
2019-09-24 23:30:23 +0800

    init

---
+++ build/precache-manifest.0cc82bc2f5020ea628c839f7e10df0f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad81a721f3ff265c9c3492e275fdf53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36c0d443b066835a7c4b",
+    "url": "/static/css/2.edebae30.chunk.css"
+  },
+  {
+    "revision": "50801a3facd35571ad13",
+    "url": "/static/css/main.6a3b3605.chunk.css"
+  },
+  {
+    "revision": "36c0d443b066835a7c4b",
+    "url": "/static/js/2.38c1879d.chunk.js"
+  },
+  {
+    "revision": "50801a3facd35571ad13",
+    "url": "/static/js/main.6f662ef5.chunk.js"
+  },
+  {
+    "revision": "bc91c1128f14d94a3dbb",
+    "url": "/static/js/runtime~main.7a618085.js"
+  },
+  {
+    "revision": "9161e238b798c444b732dbbb7420a09e",
+    "url": "/static/media/404.9161e238.png"
+  },
+  {
+    "revision": "ab45504baf89be7c7792b4426c4a6594",
+    "url": "/static/media/bg.ab45504b.jpg"
+  }
+]);
